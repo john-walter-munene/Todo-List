789aa9d607cb5c616cc398b0154f91f007826de7
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2024-06-05 10:12:17 +0300

    On developing a functional console ToDo Application.

--- dist/task.bundle.js
+++ dist/task.bundle.js
@@ -10,51 +10,20 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/notes.js":
-/*!**********************!*\
-  !*** ./src/notes.js ***!
-  \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Notes: () => (/* binding */ Notes)\n/* harmony export */ });\n// Notes generator.\r\nclass Notes {\r\n    constructor() {\r\n        this.notePoints = [];\r\n    }\r\n\r\n    addNotes(noteLead, notePoints) {\r\n        let points = {\r\n            noteLead,\r\n            notePoints,\r\n        }\r\n        this.notePoints.push(points);\r\n        return;\r\n    }\r\n}\r\n\r\n\n\n//# sourceURL=webpack://todo-list/./src/notes.js?");
-
-/***/ }),
-
 /***/ "./src/task.js":
 /*!*********************!*\
   !*** ./src/task.js ***!
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Task: () => (/* binding */ Task)\n/* harmony export */ });\n/* harmony import */ var _notes__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./notes */ \"./src/notes.js\");\n\r\n\r\n// Task generator.\r\nclass Task {\r\n    constructor(taskTitle, taskAssignee, taskStatus, taskDescription, dueDate, taskPriority,) {\r\n        this.taskTitle = taskTitle;\r\n        this.taskAssignee = taskAssignee;\r\n        this.taskStatus = taskStatus;\r\n        this.taskDescription = taskDescription;\r\n        this.dueDate = dueDate;\r\n        this.taskPriority = taskPriority;\r\n        this.taskNotes = new _notes__WEBPACK_IMPORTED_MODULE_0__.Notes();\r\n    }\r\n}\r\n\r\n// newTask.taskNotes.addNotes(\"Goals\", \"Run a developer marathon.\");\r\n// for (let key in newTask) {\r\n//     console.log(newTask[key]);\r\n// }\r\n\r\n// newTask.taskNotes.notePoints.forEach(noteItem => {\r\n//     for (let note in noteItem) console.log(noteItem[note]);\r\n// });\r\n\r\n\n\n//# sourceURL=webpack://todo-list/./src/task.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Task: () => (/* binding */ Task)\n/* harmony export */ });\n// Task generator.\r\nclass Task {\r\n    constructor(taskTitle, taskAssignee, taskStatus, taskDescription, dueDate, taskPriority,) {\r\n        this.taskTitle = taskTitle;\r\n        this.taskAssignee = taskAssignee;\r\n        this.taskStatus = taskStatus;\r\n        this.taskDescription = taskDescription;\r\n        this.dueDate = dueDate;\r\n        this.taskPriority = taskPriority;\r\n    }\r\n}\r\n\r\n\n\n//# sourceURL=webpack://todo-list/./src/task.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
@@ -90,7 +59,8 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/task.js");
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/task.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
--- src/task.js
+++ src/task.js
@@ -1,5 +1,3 @@
-import { Notes } from "./notes";
-
 // Task generator.
 class Task {
     constructor(taskTitle, taskAssignee, taskStatus, taskDescription, dueDate, taskPriority,) {
@@ -9,17 +7,7 @@ class Task {
         this.taskDescription = taskDescription;
         this.dueDate = dueDate;
         this.taskPriority = taskPriority;
-        this.taskNotes = new Notes();
     }
 }
-
-// newTask.taskNotes.addNotes("Goals", "Run a developer marathon.");
-// for (let key in newTask) {
-//     console.log(newTask[key]);
-// }
-
-// newTask.taskNotes.notePoints.forEach(noteItem => {
-//     for (let note in noteItem) console.log(noteItem[note]);
-// });
 
 export { Task };
